8fafdea8826290d1f93f60fdbb63faecd5093b58
mihir39nr@gmail.com <mihir39nr@gmail.com>
2023-09-26 00:01:00 +0530

    Added Client

---
+++ sharptalk-frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Homepage = () => {
+    return (
+        <div className="homepage-container">
+            <div>
+                <input type="email" placeholder="Enter your email"/>
+                <input type="text" placeholder="Enter room code"/>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
